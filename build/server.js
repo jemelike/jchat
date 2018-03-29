a9823fd570786828626a7e968979b5f5bf0838d3
Joseph Emelike <jemelike@rainn.com>
2018-03-28 22:41:49 -0400

    began setting up server

---
+++ build/server.js
@@ -0,0 +1,23 @@
+
+const config = require('../config')
+if (!process.env.NODE_ENV) {
+  process.env.NODE_ENV = JSON.parse(config.dev.env.NODE_ENV)
+}
+
+const opn = require('opn')
+const cors = require('cors')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const path = require('path')
+const express = require('express')
+const webpack = require('webpack')
+const proxyMiddleware = require('http-proxy-middleware')
+const webpackConfig = (process.env.NODE_ENV === 'testing' || process.env.NODE_ENV === 'production')
+  ? require('./webpack.prod.conf')
+  : require('./webpack.dev.conf')
+
+// default port where dev server listens for incoming traffic
+const port = process.env.PORT || config.dev.port
+
+// automatically open browser, if not set will be false
+const autoOpenBrowser = !!config.dev.autoOpenBrowser
