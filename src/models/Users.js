50fa67064b463f4cbfc8a32e64069176afad5a31
Joseph Emelike <jemelike@rainn.com>
2018-03-29 18:23:57 -0400

    Added a User model for login purposes

---
+++ src/models/Users.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UsersSchema = new Schema({
+    email: String,
+    name: String,
+    available: Boolean
+});
+
+var Users = mongoose.model("User", UsersSchema);
+module.exports = Users;
