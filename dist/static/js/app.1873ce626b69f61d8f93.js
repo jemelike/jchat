6380d86c76b3551654aa8a89e0beaacef6fa99c1
Joseph Emelike <jemelike@rainn.com>
2018-04-18 13:18:17 -0400

    Moved the RAINN folder and added build.js

---
+++ dist/static/js/app.1873ce626b69f61d8f93.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{"DP/h":function(e,n){},NHnr:function(e,n,t){t("PO0E"),t("DP/h")},PO0E:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default={content:"<p>The app loads here</p>"}}},["NHnr"]);
+//# sourceMappingURL=app.1873ce626b69f61d8f93.js.map
