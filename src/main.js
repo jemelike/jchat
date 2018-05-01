d15e344404daf4a59e11561e829bafa37b354c5c
Joseph Emelike <jemelike@rainn.com>
2018-05-01 16:37:10 -0400

    added auth0 for authentication. Still needs work to import auth0

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 const app = require('./components/app')
 const routes = require('./routes')
 let body = $("body")
-$(document).ready(
+
+
+window.addEventListener('load', function (e) {
+
+    var webAuth = new auth0.WebAuth({
+        domain: 'je-tech.auth0.com',
+        clientID: 'S3qDeWH5m32ZRuHY2xRmbpM2zfG1ccyB',
+        responseType: 'token id_token',
+        audience: 'https://je-tech.auth0.com/userinfo',
+        scope: 'openid',
+        redirectUri: 'localhost:8080/static/staffer.html'
+    });
     
-)
+        e.preventDefault();
+        webAuth.authorize();
+
+});
